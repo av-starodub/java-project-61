432edb8b77daea266aa56ce2b77ffd79f14022a3
Alexandr Starodubenko <alexvladstr@gmail.com>
2023-01-05 02:25:04 +0300

    Implement the game selection logic in the App class using user interaction.

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,25 @@
 package hexlet.code;
 
+import java.nio.charset.Charset;
+import java.util.Scanner;
+
+import static hexlet.code.Cli.getUserAnswer;
+import static hexlet.code.Cli.greet;
+import static hexlet.code.Cli.print;
+import static hexlet.code.Cli.showMenu;
+
 public class App {
     public static void main(String[] args) {
-        Cli.greet();
+        var sc = new Scanner(System.in, Charset.defaultCharset());
+        showMenu();
+        switch (getUserAnswer(sc)) {
+            case "1" -> greet(sc);
+            case "2" -> runGame(sc);
+            case "0" -> print("See you!\n");
+            default -> print("");
+        }
+    }
+    private static void runGame(Scanner sc) {
+        greet(sc);
     }
 }
--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -2,12 +2,31 @@ package hexlet.code;
 
 import java.util.Scanner;
 
-public class Cli {
-    public static void greet() {
-        var sc = new Scanner(System.in);
-        System.out.println("Welcome to the Brain Games!");
-        System.out.print("May I have your name?");
-        var userName = sc.nextLine();
-        System.out.printf("Hello, %s!\n", userName);
+public final class Cli {
+    public static void greet(Scanner sc) {
+        print("""
+                Welcome to the Brain Games!
+                May I have your name?\s""");
+        print(String.format("Hello, %s!\n", getUserAnswer(sc)));
+    }
+
+    public static void showMenu() {
+        print(createMenu());
+    }
+
+    private static String createMenu() {
+        return """
+                Please enter the game number and press Enter.
+                1 - Greet
+                0 - Exit
+                Your choice:\s""";
+    }
+
+    public static void print(String message) {
+        System.out.print(message);
+    }
+
+    public static String getUserAnswer(Scanner sc) {
+        return sc.nextLine();
     }
 }
