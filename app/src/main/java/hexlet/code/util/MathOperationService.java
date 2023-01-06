3a566101cf864268d2acc1c9c6488bbe4e179b31
Alexandr Starodubenko <alexvladstr@gmail.com>
2023-01-06 07:38:36 +0300

    Simplify findGCD method.

--- app/src/main/java/hexlet/code/util/MathOperationService.java
+++ app/src/main/java/hexlet/code/util/MathOperationService.java
@@ -12,8 +12,8 @@ public final class MathOperationService {
         if (secondNumber == 0 && firstNumber == 0) {
             throw new IllegalArgumentException("Both arguments are zero. ");
         }
-        if (secondNumber == 0 || firstNumber == 0) {
-            return Math.abs(firstNumber + secondNumber);
+        if (secondNumber == 0) {
+            return Math.abs(firstNumber);
         }
         int reminder = firstNumber % secondNumber;
         return reminder == 0 ? secondNumber : findGCD(secondNumber, reminder);
