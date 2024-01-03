8b5737e6cc1c408c4871eabf3022cb8b0a82eebc
Alexandr Starodubenko <alexvladstr@gmail.com>
2024-01-03 23:46:16 +0300

    Remove magic numbers from IS_PRIME.check() method

--- app/src/main/java/hexlet/code/math/operations/CheckOperationType.java
+++ app/src/main/java/hexlet/code/math/operations/CheckOperationType.java
@@ -8,13 +8,17 @@ public enum CheckOperationType {
         }
     },
     IS_PRIME {
+        private static final int MIN_DIVISOR = 3;
+        private static final int START_PRIME = 5;
+        private static final int STEP = 6;
+
         @Override
         public boolean check(int number) {
-            var notEven = number % 2 != 0;
-            var notDivisibleByThree = number % 3 != 0;
+            var notEven = IS_EVEN.check(number);
+            var notDivisibleByThree = number % MIN_DIVISOR != 0;
             var result = number >= 1 && notEven && notDivisibleByThree;
-            if (number > 7 && result) {
-                for (var divisor = 5; divisor < Math.sqrt(number); divisor += 6) {
+            if (number > START_PRIME && result) {
+                for (var divisor = START_PRIME; divisor < Math.sqrt(number); divisor += STEP) {
                     if (number % divisor == 0 || number % (divisor + 2) == 0) {
                         result = false;
                         break;
